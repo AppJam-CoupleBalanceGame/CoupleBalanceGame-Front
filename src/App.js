014ec1e3c26cfec678044d606bb71ea00079831b
boram065 <boram4784@gmail.com>
2024-04-21 03:22:04 +0900

    Feat: 밸런스게임 시간 설정 화면 구현

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SplashPage from './pages/SplashPage';
-import LoginPage from './pages/LoginPage';
-import LoginPopupPage from './pages/LoginPopupPage';
+import Splash from './pages/SplashPage';
+import Login from './pages/LoginPage';
+import LoginPopup from './pages/LoginPopupPage';
 import ProfileSetting from './pages/ProfileSettingPage';
+import Profile from './pages/ProfilePage';
 import CoupleInvite from './pages/CoupleInvitePage';
 import BalanceGame from './pages/BalanceGamePage';
+import BalanceGameTime from './pages/BalanceGameTime';
 
 function App() {
   return(
       <Router>
           <Routes>
-              <Route path="/" element={<SplashPage />}/>
-              <Route path="/login" element={<LoginPage />}/>
-              <Route path="/loginPopup" element={<LoginPopupPage />} />
+              <Route path="/" element={<Splash />}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/loginPopup" element={<LoginPopup />} />
+
               <Route path="/profile-setting" element={<ProfileSetting />} />
+              <Route path='/profile' element={<Profile />} />
+              
               <Route path="/couple-invite" element={<CoupleInvite />} />
+
               <Route path='/balance-game' element={<BalanceGame />} />
+              <Route path='/balance-game-time' element={<BalanceGameTime />} />
           </Routes>
       </Router>
   )
