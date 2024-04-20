e1e961fe8f38abe200b877478c2bc0db1cc188c0
rkdbs <w2207@e-mirim.hs.kr>
2024-04-20 20:49:40 +0900

    Feat: loginpopup 화면 구현

---
+++ src/pages/LoginPopupPage.js
@@ -0,0 +1,11 @@
+import LoginPopup from "../components/login/LoginPopup";
+
+function LoginPopupPage() {
+    return (
+        <>
+            <LoginPopup />
+        </>
+    )
+}
+
+export default LoginPopupPage;
