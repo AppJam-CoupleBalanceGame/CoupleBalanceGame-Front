48cf7a40ea2c71da6483276c511da7116f33f5fd
rkdbs <w2207@e-mirim.hs.kr>
2024-04-21 03:21:23 +0900

    Feat: 이전 내용 상세 화면 구현

---
+++ src/pages/BalanceGame3Page.js
@@ -0,0 +1,11 @@
+import BalanceGame3 from "../components/balanceGame/BalanceGame3";
+
+function BalanceGame3Page() {
+    return (
+        <>
+            <BalanceGame3 />
+        </>
+    )
+}
+
+export default BalanceGame3Page;
