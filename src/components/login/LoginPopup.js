8d28e4030b140b6bc1de0633eb58a5c94f4358fe
rkdbs <w2207@e-mirim.hs.kr>
2024-04-20 21:14:49 +0900

    Refactor: 멘트 수정

--- src/components/login/LoginPopup.js
+++ src/components/login/LoginPopup.js
@@ -10,12 +10,12 @@ function LoginPopup() {
                 <p className={loginStyle['title']}>고객센터</p>
             </div>
             <div className={loginStyle['body']}>
-                <p className={loginStyle['question']}>Q. 이미 계정이 있는데 어떡하오?</p>
+                <p className={loginStyle['question']}>Q. 이미 계정이 있는데 어떡하나요?</p>
                 <p className={loginStyle['answer']}>
                     저희 엘레강스 커플 앱의 로그인 및 회원가입은<br/>
-                    모두 메인창 시작하기 버튼으로 가능하오!<br/><br/>
+                    모두 메인창 시작하기 버튼으로 가능합니다.<br/><br/>
                     만약 계정을 찾을 수 없다면 오른쪽 하단의<br/>
-                    채팅 문의를 통해 연락주시오!
+                    채팅 문의를 통해 연락주세요!
                 </p>
             </div>
         </div>
