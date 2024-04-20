45457a379bca7d5e4036de9351888e1676c3d20b
rkdbs <w2207@e-mirim.hs.kr>
2024-04-21 03:16:40 +0900

    Refactor: link 삭제

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,13 @@
 import '../../styles/common/Style.css';
 import style from '../../styles/common/Header.module.css';
 
-import { Link } from 'react-router-dom';
-
 import { FaArrowLeft } from "react-icons/fa6";
 
 function Header({ text }) {
     return (
         <>
             <div className={style['header']}>
-                <Link to="/login" style={{textDecoration: 'none', color: 'black'}}> <FaArrowLeft className={style['left-icon']} /> </Link>
+                <FaArrowLeft className={style['left-icon']} />
                 <p className={style['title']}>{text}</p>
             </div>
         </>
