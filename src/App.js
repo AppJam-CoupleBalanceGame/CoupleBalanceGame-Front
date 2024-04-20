15e2a5c921ac134c9b7fcf8af82a7736d2a6489d
boram065 <boram4784@gmail.com>
2024-04-20 22:37:18 +0900

    Feat: 프로필 등록 화면 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
 import LoginPage from './pages/LoginPage';
 import LoginPopupPage from './pages/LoginPopupPage';
+import ProfileSetting from './components/profile/ProfileSetting';
 
 function App() {
   return(
@@ -11,6 +12,7 @@ function App() {
               <Route path="/" element={<SplashPage />}/>
               <Route path="/login" element={<LoginPage />}/>
               <Route path="/loginPopup" element={<LoginPopupPage />} />
+              <Route path="/profile-setting" element={<ProfileSetting />} />
           </Routes>
       </Router>
   )
